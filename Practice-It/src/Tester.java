cf7ac81b8eaf8c624549d6b55c846eab511cbdf9
Cobbleopolis <agounimon@yahoo.com>
2015-05-06 21:10:16 -0400

    Finished grading period

--- Practice-It/src/Tester.java
+++ Practice-It/src/Tester.java
@@ -1,22 +1,6 @@
-import java.util.Scanner;
-
 public class Tester {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter an integer:");
-		int num = Integer.parseInt(keyboard.next());
-		System.out.println(num + " is an " + EvenOrOdd.evenOrOdd(num) + " number.");
-	}
-}
 
-class EvenOrOdd {
-
-	public static String evenOrOdd(int number) {
-		if(number % 2 == 1){
-			return "odd";
-		} else {
-			return "even";
-		}
 	}
 }
